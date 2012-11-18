74604f0ff0714717029c116070a505e2c25c0ca4
shauney <shauney@shauney-laptop.(none)>
2012-11-18 15:18:17 +0000

    Added new project

---
+++ CppTest/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: shauney
+ *
+ * Created on 18 November 2012, 15:16
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
